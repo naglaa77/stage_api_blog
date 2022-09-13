5314b444788b4c176d0dfcafc11c491b9b526791
naglaa77 <sama_hap@yahoo.com>
2022-09-13 17:37:07 +0100

    add paramconverter

--- src/Controller/PostsController.php
+++ src/Controller/PostsController.php
@@ -7,19 +7,32 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Serializer\SerializerInterface;
 
 class PostsController extends AbstractController
 {
     #[Route('/api/posts', name: 'posts',methods: ['GET'])]
-    public function getPostList(PostsRepository $postsRepository): JsonResponse
+    public function getPostList(PostsRepository $postsRepository,SerializerInterface $serializer): JsonResponse
     {
 
         $postList = $postsRepository->findAll()  ;  
-    
-        return new JsonResponse([
-        
-            'posts' => $postList,
-        
-        ]);
+        $jsonPostList = $serializer->serialize( $postList, 'json');
+
+        return new JsonResponse($jsonPostList, Response::HTTP_OK,[],true);
+    }
+
+
+    // for single post
+    #[Route('/api/posts/{id}' , name: 'detailPost', methods: ['GET'])]
+   public  function getDetailePost(int $id , PostsRepository $postsRepository, SerializerInterface $serializer):JsonResponse
+    {
+
+        $post = $postsRepository->find($id);
+        if ($post) {
+
+            $jsonPost = $serializer->serialize($post, 'json');
+            return new JsonResponse($jsonPost, Response::HTTP_OK, [], true);
+        }
+         return new JsonResponse(null, Response::HTTP_NOT_FOUND);
     }
 }
